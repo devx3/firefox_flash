c772015b4d91aacc3184c2758a20e14230181fd7
Erick Bruno <erickfabiani123@gmail.com>
2017-09-01 10:19:55 -0300

    Show proccess added

--- install-ff.sh
+++ install-ff.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 INSTALL_FF_FOLDER=install-ff
 FLASHP_VERSION="26.0.0.151"
 FIREFOX_VERSION="55.0.3"
@@ -8,14 +8,22 @@ if [ ! -d ./$INSTALL_FF_FOLDER ]; then
 	mkdir ./$INSTALL_FF_FOLDER
 fi
 
+echo "Downloading Flash Player Plugin..."
 wget -P ./$INSTALL_FF_FOLDER https://fpdownload.adobe.com/pub/flashplayer/pdc/$FLASHP_VERSION/flash_player_ppapi_linux.$BASESYSTEM.tar.gz
 
+echo "Downloading Firefox..."
 wget -P ./$INSTALL_FF_FOLDER https://download-installer.cdn.mozilla.net/pub/firefox/releases/$FIREFOX_VERSION/linux-$BASESYSTEM/pt-BR/firefox-$FIREFOX_VERSION.tar.bz2
 
 # installing flashplayer 
+
 tar xvzf ./$INSTALL_FF_FOLDER/flash_player_ppapi_linux.$BASESYSTEM.tar.gz -C $INSTALL_FF_FOLDER
+
+sleep 1
+echo "installing libs..."
 sudo cp -rf ./$INSTALL_FF_FOLDER/libpepflashplayer.so /usr/lib/mozilla/plugins
 sudo cp -rf ./$INSTALL_FF_FOLDER/usr/* /usr/
 
+sleep 1
+echo "Extracting firefox..."
 tar xvjf ./$INSTALL_FF_FOLDER/firefox-$FIREFOX_VERSION.tar.bz2 -C $INSTALL_FF_FOLDER
 
